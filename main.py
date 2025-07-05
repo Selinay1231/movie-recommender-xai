b21ec1ce8917102e36bb8f47ea10dbf99223f906
Selinay1231 <109960656+Selinay1231@users.noreply.github.com>
2025-07-05 19:33:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -208,7 +208,7 @@ download_from_gdrive("1M0v8mSSbgS7Wz1HoMdCM_YqpXTh0bGd9", "./data/genome-scores.
 def load_data():
     base_path = "./data/"
     movies = pd.read_csv(base_path + "movies.csv", sep=";")
-    ratings = pd.read_csv(base_path + "ratings.csv", sep=",", encoding="utf-8")
+    ratings = pd.read_csv(base_path + "ratings.csv", sep=";", encoding="utf-8")
     # Zeige die ersten 10 Zeilen der ratings-Daten an
     st.write(ratings.head(10))
     movies["year"] = movies["title"].str.extract(r"\((\d{4})\)").astype(float)
